5d2c4242b8540dc0ad565364a0b42b35c9d994d6
John <liuyuqiang@pronetway.com>
2024-08-06 15:24:05 +0800

    version number fix

--- get_version.sh
+++ get_version.sh
@@ -4,7 +4,7 @@ function download() {
     echo "try to get the version: ${1} ..."
     rm json.hpp
     local latest_tag="$(curl "https://api.github.com/repos/nlohmann/json/tags" | jq -r '.[0].name')"
-    wget -c "https://github.com/nlohmann/json/releases/download/v${latest_tag}/json.hpp"
+    wget -c "https://github.com/nlohmann/json/releases/download/${latest_tag}/json.hpp"
 }
 
 download ${1}
