974daf333da7bc89481f93ac257b515b0b21424d
John <liuyuqiang@pronetway.com>
2024-08-06 15:43:53 +0800

    fix wording

--- get_version.sh
+++ get_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function download() {
-    echo "try to get the version: ${1} ..."
+    echo "try to get the latest version..."
     rm json.hpp
     local latest_tag="$(curl "https://api.github.com/repos/nlohmann/json/tags" | jq -r '.[0].name')"
     wget -c "https://github.com/nlohmann/json/releases/download/${latest_tag}/json.hpp"
 }
 
-download ${1}
+download
